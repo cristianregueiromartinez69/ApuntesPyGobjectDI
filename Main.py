7b9ddf9133c578b0f1e75104d9fe84aec90ccb23
Cristian <cregueiromartinez@danielcastelao.org>
2024-12-13 13:16:04 +0100

    poniendo mensajes

--- Main.py
+++ Main.py
@@ -6,10 +6,15 @@ class FiestraPrincipal(Gtk.Window):
     def __init__(self):
         super().__init__()
 
-        self.set_size_request(200, 200)
+        self.set_size_request(200, 200) #definimos el tamaño de la ventana
+        self.set_resizable(False) #indicamos que no se puede estirar la ventana
 
-        self.caja = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=10)
+        self.caja = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=10) #declaramos el layout, que será vertical
         self.add(self.caja)
+
+        '''
+        declaramos los label, botones y texto
+        '''
 
         self.lbSaudo = Gtk.Label(label="Nombre: ")
         self.caja.pack_start(self.lbSaudo, True, True, 0)
@@ -20,8 +25,8 @@ class FiestraPrincipal(Gtk.Window):
         self.btnSaudo = Gtk.Button(label="Pulsame")
         self.caja.pack_start(self.btnSaudo, True, True, 0)
 
-        self.btnSaudo.connect("clicked", self.saludo_boton)
-        self.show_all()
+        self.btnSaudo.connect("clicked", self.saludo_boton) #conectamos el boton con una funcion
+        self.show_all() #mostramos todo
 
 
     def saludo_boton(self, button):
@@ -33,5 +38,5 @@ class FiestraPrincipal(Gtk.Window):
 
 if __name__ == '__main__':
     win = FiestraPrincipal()
-    win.connect("destroy", Gtk.main_quit)  # Cerrar la ventana
+    win.connect("destroy", Gtk.main_quit)  #si cerramos la pp, se cierra todo
     Gtk.main()
